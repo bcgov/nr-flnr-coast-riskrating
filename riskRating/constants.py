cb2ddf836579c32206e8b9571f3e7181122bca02
Chan <Keith.1.Chan@gov.bc.ca>
2021-06-10 08:54:01 -0700

    Added Python scripts

---
+++ riskRating/constants.py
@@ -0,0 +1,13 @@
+import os
+import dotenv
+
+envPath = os.path.join(os.path.dirname(__file__), '..', '.env')
+if os.path.exists(envPath):
+    print("loading dot env...")
+    dotenv.load_dotenv()
+
+ORACLE_USERNAME = os.environ['ORACLE_USERNAME']
+ORACLE_PASSWORD = os.environ['ORACLE_PASSWORD']
+ORACLE_TNS_NAME = os.environ['ORACLE_TNS_NAME']
+ORACLE_DRIVER_NAME = os.environ['ORACLE_DRIVER_NAME']
+ROOT_PATH = os.environ['ROOT_PATH']
